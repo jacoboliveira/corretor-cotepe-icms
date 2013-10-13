094bb266bc4de41fad55b762dca02a21d63347ab
Jacob <jacoblisboa@gmail.com>
2013-10-13 08:34:53 -0700

    ajuste do enconding

--- src/br/corretor/cotepe/icms/Main.java
+++ src/br/corretor/cotepe/icms/Main.java
@@ -9,12 +9,12 @@ import br.corretor.cotepe.icms.entities.TipoE14;
 import br.corretor.cotepe.icms.util.FileHelper;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
-import org.apache.commons.lang.StringUtils;
 
 /**
  *
@@ -33,9 +33,9 @@ public class Main {
 
         //   Locale.setDefault(new Locale("pt","BR"));
         File arquivo = new File("c:/teste/TDMBE051275610000142726_20131007_081413.TXT");
+        String enconding = "ISO-8859-1";
+        List<String> linhas = FileHelper.readLines(arquivo, enconding);
 
-        List<String> linhas = FileHelper.readLines(arquivo, "ISO-8859-1");
-        StringBuilder conteudo = new StringBuilder();
         List<ReducoesZ> reducoesZList = new ArrayList<ReducoesZ>();
         List<TipoE14> e14cooList = new ArrayList<TipoE14>();
 
@@ -46,53 +46,60 @@ public class Main {
                 reducoesZ.setCoo(linha.substring(52, 58));
                 reducoesZ.setDataMovimento(linha.substring(64, 72));
                 reducoesZList.add(reducoesZ);
-                //System.out.println(linha.substring(52, 58));                
+//                System.out.println(linha.substring(52, 58));
             }
-            //System.out.println(linha);
+////            System.out.println(linha);
         }
         for (ReducoesZ reducoesZ : reducoesZList) {
+            StringBuilder conteudo = new StringBuilder();
+
             for (String linha : linhas) {
 
                 if (linha.substring(0, 3).equals("E14")) {
                     String dataVerificar = linha.substring(58, 66);
                     String e14Coo = linha.substring(52, 58);
+                    //System.out.println(reducoesZ.getDataMovimento());
+                    if (dataVerificar.equals(reducoesZ.getDataMovimento())) {
+                        if (!reducoesZ.getCoo().equals(e14Coo)) {
+                            StringBuilder linhaAlt = new StringBuilder(linha);
+                            linhaAlt.replace(52, 58 , reducoesZ.getCoo());
+                            linha = linhaAlt.toString();
 
-                    if (dataVerificar.equals(reducoesZ.getDataMovimento())
-                            && !reducoesZ.getCoo().equals(e14Coo)) {
-                        StringBuilder linhaAlt = new StringBuilder(linha);
-                        linhaAlt.replace(53, 58 + 1, String.valueOf(reducoesZ.getCoo()));
-                        linha = linhaAlt.toString();
-                        String ccf = linha.substring(46, 52);
-                        String coo = linha.substring(52, 58);
-                        TipoE14 tipoE14Comparar = new TipoE14();
-                        tipoE14Comparar.setCcf(ccf);
-                        tipoE14Comparar.setCoo(coo);
-                        if (!isContem(tipoE14Comparar, e14cooList)) {
-                            TipoE14 tipoE14 = new TipoE14();
-                            tipoE14.setCcf(ccf);
-                            tipoE14.setCoo(coo);
-                            e14cooList.add(tipoE14);
+                            String ccf = linha.substring(46, 52);
+                            String coo = linha.substring(52, 58);
+                            TipoE14 tipoE14Comparar = new TipoE14();
+                            tipoE14Comparar.setCcf(ccf);
+                            tipoE14Comparar.setCoo(coo);
+                            if (!isContem(tipoE14Comparar, e14cooList)) {
+                                TipoE14 tipoE14 = new TipoE14();
+                                tipoE14.setCcf(ccf);
+                                tipoE14.setCoo(coo);
+                                e14cooList.add(tipoE14);
+                            }
                         }
-                        //System.out.println(reducoesZ.getCoo());
+////                        System.out.println(reducoesZ.getCoo());
                     }
                 }
                 conteudo.append(linha).append("\n");
             }
-            BufferedWriter writer = new BufferedWriter(new FileWriter(arquivo));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(arquivo), enconding));
             writer.write(conteudo.toString());
             writer.flush();
-//            writer.close();
-            //registra
-            //System.out.println(conteudo.toString());
-            // break;
+            writer.close();
+
+////            registra
+////            System.out.println(conteudo.toString());
+//             break;
         }
         for (TipoE14 tipoE14 : e14cooList) {
-            System.out.println(tipoE14.getCoo());
+        //    System.out.println(tipoE14.getCoo());
         }
-    
-        
-        StringBuilder passo2Conteudo = new StringBuilder();
+
+
+
         for (TipoE14 tipoE14 : e14cooList) {
+            StringBuilder conteudo = new StringBuilder();
             for (String linha : linhas) {
                 if (linha.substring(0, 3).equals("E15")) {
 
@@ -100,14 +107,15 @@ public class Main {
                     if (ccf.equals(tipoE14.getCcf())) {
                         StringBuilder alterarLinha = new StringBuilder(linha);
                         alterarLinha.replace(47, 52 + 1, tipoE14.getCoo());
-                        linha = alterarLinha.toString();                        
+                        linha = alterarLinha.toString();
                     }
 
                 }
-                passo2Conteudo.append(linha).append("\n");
+                conteudo.append(linha).append("\n");
             }
-            BufferedWriter writer = new BufferedWriter(new FileWriter(arquivo));
-            writer.write(passo2Conteudo.toString());
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(arquivo), enconding));
+            writer.write(conteudo.toString());
             writer.flush();
             writer.close();
         }
