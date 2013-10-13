3c1a1fd2f912e53428243a1b5ce01d5516ee12df
Jacob <jacoblisboa@gmail.com>
2013-10-13 13:54:46 -0700

    correcao do carregamento dos arquivos  novamente

--- src/br/corretor/cotepe/icms/Main.java
+++ src/br/corretor/cotepe/icms/Main.java
@@ -10,11 +10,12 @@ import br.corretor.cotepe.icms.util.FileHelper;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Formatter;
 import java.util.List;
+import java.util.Locale;
 
 /**
  *
@@ -33,7 +34,7 @@ public class Main {
 
         //   Locale.setDefault(new Locale("pt","BR"));
         File arquivo = new File("c:/teste/TDMBE051275610000142726_20131007_081413.TXT");
-        String enconding = "ISO-8859-1";
+        String enconding = "UTF-8";//"ISO-8859-1"
         List<String> linhas = FileHelper.readLines(arquivo, enconding);
 
         List<ReducoesZ> reducoesZList = new ArrayList<ReducoesZ>();
@@ -52,31 +53,32 @@ public class Main {
         }
         for (ReducoesZ reducoesZ : reducoesZList) {
             StringBuilder conteudo = new StringBuilder();
-
+            linhas = FileHelper.readLines(arquivo, enconding);
             for (String linha : linhas) {
-
+                //Formatter linha = new Formatter(new StringBuffer(saida), new Locale("pt", "BR"));
+                
                 if (linha.substring(0, 3).equals("E14")) {
                     String dataVerificar = linha.substring(58, 66);
                     String e14Coo = linha.substring(52, 58);
                     //System.out.println(reducoesZ.getDataMovimento());
-                    if (dataVerificar.equals(reducoesZ.getDataMovimento())) {
-                        if (!reducoesZ.getCoo().equals(e14Coo)) {
-                            StringBuilder linhaAlt = new StringBuilder(linha);
-                            linhaAlt.replace(52, 58 , reducoesZ.getCoo());
-                            linha = linhaAlt.toString();
+                    if (dataVerificar.equals(reducoesZ.getDataMovimento()) && !reducoesZ.getCoo().equals(e14Coo)) {
 
-                            String ccf = linha.substring(46, 52);
-                            String coo = linha.substring(52, 58);
-                            TipoE14 tipoE14Comparar = new TipoE14();
-                            tipoE14Comparar.setCcf(ccf);
-                            tipoE14Comparar.setCoo(coo);
-                            if (!isContem(tipoE14Comparar, e14cooList)) {
-                                TipoE14 tipoE14 = new TipoE14();
-                                tipoE14.setCcf(ccf);
-                                tipoE14.setCoo(coo);
-                                e14cooList.add(tipoE14);
-                            }
+                        StringBuilder linhaAlt = new StringBuilder(linha);
+                        linhaAlt.replace(52, 58, reducoesZ.getCoo());
+                        linha = linhaAlt.toString();
+
+                        String ccf = linha.substring(46, 52);
+                        String coo = linha.substring(52, 58);
+                        TipoE14 tipoE14Comparar = new TipoE14();
+                        tipoE14Comparar.setCcf(ccf);
+                        tipoE14Comparar.setCoo(coo);
+                        if (!isContem(tipoE14Comparar, e14cooList)) {
+                            TipoE14 tipoE14 = new TipoE14();
+                            tipoE14.setCcf(ccf);
+                            tipoE14.setCoo(coo);
+                            e14cooList.add(tipoE14);
                         }
+
 ////                        System.out.println(reducoesZ.getCoo());
                     }
                 }
@@ -93,20 +95,21 @@ public class Main {
 //             break;
         }
         for (TipoE14 tipoE14 : e14cooList) {
-        //    System.out.println(tipoE14.getCoo());
+                System.out.println(tipoE14.getCoo());
         }
 
 
-
+        
         for (TipoE14 tipoE14 : e14cooList) {
             StringBuilder conteudo = new StringBuilder();
+            linhas = FileHelper.readLines(arquivo, enconding);
             for (String linha : linhas) {
                 if (linha.substring(0, 3).equals("E15")) {
 
                     String ccf = linha.substring(52, 58);
                     if (ccf.equals(tipoE14.getCcf())) {
                         StringBuilder alterarLinha = new StringBuilder(linha);
-                        alterarLinha.replace(47, 52 + 1, tipoE14.getCoo());
+                        alterarLinha.replace(46, 52, tipoE14.getCoo());
                         linha = alterarLinha.toString();
                     }
 
@@ -124,7 +127,7 @@ public class Main {
 
     private static boolean isContem(TipoE14 tipoE14Comparar, List<TipoE14> e14cooList) {
         for (TipoE14 tipoE14 : e14cooList) {
-            if (tipoE14.getCoo().equals(tipoE14Comparar.getCoo())) {
+            if (tipoE14.getCcf().equals(tipoE14Comparar.getCcf())) {
                 return true;
             }
         }
